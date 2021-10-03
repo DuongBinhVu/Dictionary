65c1896506c8cdec75ee9f5ef47fd26985a8c65c
duongdaydungso <vietnamminer2002@gmail.com>
2021-10-03 11:14:55 +0700

    feat: change and add new methods
    - Change Word variables name for correct semantics.
    - Add Word comparison methods.
    - Add action for Management.
    - Modified dictionaryAdvanced.
    - Add resource folder.

--- src/dictionary/DictionaryCommandline.java
+++ src/dictionary/DictionaryCommandline.java
@@ -1,27 +1,71 @@
 package dictionary;
 
+import java.util.Scanner;
+
 public class DictionaryCommandline extends DictionaryManagement {
-  /** Print list of words. */
-  public void showAllWords() {
-    System.out.printf("%-10s %-30s %-30s%n", "No", "| English", "| Vietnamese");
+    /**
+     * Print list of words.
+     */
+    public void showAllWords() {
+        System.out.printf("%-10s %-30s %-30s%n", "No", "| English", "| Vietnamese");
 
-    for (int i = 0; i < wordList.size(); i++) {
-      System.out.printf(
-          "%-10d %-30s %-30s%n",
-          (i + 1), "| " + wordList.get(i).getWordTarget(), "| " + wordList.get(i).getWordExplain());
+        for (int i = 0; i < wordList.size(); i++) {
+            System.out.printf(
+                    "%-10d %-30s %-30s%n",
+                    (i + 1), "| " + wordList.get(i).getTrueForm(), "| " + wordList.get(i).getMeaning());
+        }
     }
-  }
 
-  /** Basic version. */
-  public void dictionaryBasic() {
-    insertFromCommandline();
-    showAllWords();
-  }
+    /**
+     * Showing management functions.
+     */
+    public void showHelp() {
+        System.out.println("show         Show all words in current dictionary");
+        System.out.println("add          Add a new word to dictionary");
+        System.out.println("remove       Remove a word from dictionary");
+        System.out.println("edit         Edit a word in dictionary");
+        System.out.println("search       Search and show all words start with ...");
+        System.out.println("import       Import dictionary data from file");
+        System.out.println("export       Export current dictionary to file");
+        System.out.println("help         Show all commands");
+        System.out.println("exit         Exit program");
+    }
 
-  /** Advanced version. */
-  public void dictionaryAdvanced() {
-    insertFromFile();
-    showAllWords();
-    dictionaryLookup();
-  }
+    /**
+     * Advanced version.
+     */
+    public void dictionaryAdvanced() {
+        Scanner scanner = new Scanner(System.in);
+
+        runningDictionaryAdvanced:
+        while (true) {
+            String command = scanner.nextLine();
+
+            switch (command) {
+                case "show" -> showAllWords();
+                case "help" -> showHelp();
+                case "exit" -> {
+                    System.out.println("Exiting program!");
+                    break runningDictionaryAdvanced;
+                }
+                default -> {
+                    CommandType order;
+
+                    order = switch (command) {
+                        case "add" -> CommandType.ADD;
+                        case "remove" -> CommandType.REMOVE;
+                        case "edit" -> CommandType.EDIT;
+                        case "search" -> CommandType.SEARCH;
+                        case "import" -> CommandType.IMPORT;
+                        case "export" -> CommandType.EXPORT;
+                        default -> CommandType.NO_COMMAND;
+                    };
+
+                    if (order != CommandType.NO_COMMAND) {
+                        executeOrder(order);
+                    }
+                }
+            }
+        }
+    }
 }
--- src/dictionary/Word.java
+++ src/dictionary/Word.java
@@ -1,48 +1,154 @@
 package dictionary;
 
+import java.util.Scanner;
+
 public class Word {
+    // *** Class Variables ***
     /**
-     * New word.
+     * Spelling of the word (lower case).
      */
-    private String wordTarget;
+    private String spelling;
 
     /**
-     * Word's explanation.
+     * Meaning of the word.
      */
-    private String wordExplain;
+    private String meaning;
 
-    public void setWordTarget(String wordTarget) {
-        this.wordTarget = wordTarget;
+    /**
+     * Spelling of the word (true form - input form).
+     */
+    private String trueForm;
+
+    // *** Setter, Getter Methods ***
+
+    public void setSpelling(String spelling) {
+        this.spelling = spelling;
     }
 
-    public void setWordExplain(String wordExplain) {
-        this.wordExplain = wordExplain;
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
     }
 
-    public String getWordTarget() {
-        return this.wordTarget;
+    public void setTrueForm(String trueForm) {
+        this.trueForm = trueForm;
     }
 
-    public String getWordExplain() {
-        return this.wordExplain;
+    public String getSpelling() {
+        return this.spelling;
     }
+
+    public String getMeaning() {
+        return this.meaning;
+    }
+
+    public String getTrueForm() {
+        return this.trueForm;
+    }
+
+    // *** Class Methods ***
 
     /**
      * Constructor.
      */
     public Word() {
-        this.wordTarget = "";
-        this.wordExplain = "";
+        this.spelling = "";
+        this.meaning = "";
+        this.trueForm = "";
     }
 
     /**
      * Constructor with parameters.
      *
-     * @param wordTarget  new word
-     * @param wordExplain word's explanation
+     * @param trueForm input spelling of the word
+     * @param meaning  meaning of the word
      */
-    public Word(String wordTarget, String wordExplain) {
-        this.wordTarget = wordTarget;
-        this.wordExplain = wordExplain;
+    public Word(String trueForm, String meaning) {
+        this.meaning = meaning;
+        this.trueForm = trueForm;
+        this.spelling = trueForm.toLowerCase();
+    }
+
+    /**
+     * Read word from screen.
+     */
+    public void readWord() {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] reader = scanner.nextLine().split("\t");
+
+        this.trueForm = reader[0];
+        this.meaning = reader[1];
+        this.spelling = this.trueForm.toLowerCase();
+    }
+
+    /**
+     * Read word from screen.
+     *
+     * @param scanner Scanner
+     */
+    public void readWord(Scanner scanner) {
+        String[] reader = scanner.nextLine().split("\t");
+
+        this.trueForm = reader[0];
+        this.meaning = reader[1];
+        this.spelling = this.trueForm.toLowerCase();
+    }
+
+    /**
+     * Write word to screen.
+     */
+    public void writeWord() {
+        System.out.println(trueForm + "\t" + meaning);
+    }
+
+    /**
+     * Check this word spelling.
+     *
+     * @param spelling input spelling
+     * @return if same or not
+     */
+    public Boolean isSpelling(String spelling) {
+        return this.spelling.equals(spelling);
+    }
+
+    /**
+     * Compare two words spelling.
+     *
+     * @param thatWord input word
+     * @return if same or not
+     */
+    public Boolean isSpelling(Word thatWord) {
+        return this.spelling.equals(thatWord.getSpelling());
+    }
+
+    /**
+     * Check this word meaning.
+     *
+     * @param meaning input meaning
+     * @return if same or not
+     */
+    public Boolean isMeaning(String meaning) {
+        return this.meaning.equals(meaning);
+    }
+
+    /**
+     * Compare two words meaning.
+     *
+     * @param thatWord input word
+     * @return if same or not
+     */
+    public Boolean isMeaning(Word thatWord) {
+        return this.meaning.equals(thatWord.getMeaning());
+    }
+
+    /**
+     * Compare two words are similar or not.
+     *
+     * @param thatWord input word
+     * @return if same or not
+     */
+    public Boolean isEquals(Word thatWord) {
+        return this.spelling.equals(thatWord.getSpelling())
+                && this.meaning.equals(thatWord.getMeaning());
     }
 }
