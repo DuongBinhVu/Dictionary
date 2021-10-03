47170c87cb1b60c18ed4d9d0f3b4b689a3e27ba4
DuongBinhVu <20020008@vnu.edu.vn>
2021-10-03 10:46:53 +0700

    Dictionary Command Line basic version

---
+++ src/dictionary/Dictionary.java
@@ -0,0 +1,10 @@
+package dictionary;
+
+import java.util.ArrayList;
+
+public class Dictionary {
+    /**
+     * List of words.
+     */
+    protected ArrayList<Word> wordList = new ArrayList<>();
+}
